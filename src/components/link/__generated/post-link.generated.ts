84d66b25001976f90515363e0966f8ffb8774676
Matt van Voorst <mattvanvoorst@contentful.com>
2022-10-11 16:37:43 +0200

    chore: [PLATO-77] GraphQL refactoring sitemap - Refactored the sitemap to RQ - Refactored the category-link component; it was still using a manually created interface and not one based on GQL. - PageLink, PostLink and CategoryLink now all include a page/category/post name as well in their response - the sitemap is based on three aformentioned interfaces

--- src/components/link/__generated/post-link.generated.ts
+++ src/components/link/__generated/post-link.generated.ts
@@ -1,11 +1,12 @@
 import * as Types from '../../../lib/__generated/graphql.types';
 
-export type PostLinkFieldsFragment = { __typename: 'Post', slug?: string | null, sys: { __typename?: 'Sys', id: string } };
+export type PostLinkFieldsFragment = { __typename: 'Post', slug?: string | null, postName?: string | null, sys: { __typename?: 'Sys', id: string } };
 
 export const PostLinkFieldsFragmentDoc = `
     fragment PostLinkFields on Post {
   __typename
   slug
+  postName
   sys {
     id
   }
