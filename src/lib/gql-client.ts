7339f4ee978a38969b0054e1596ac6bdb10957d5
Matt van Voorst <mattvanvoorst@contentful.com>
2022-10-20 10:22:47 +0200

    fix: [] translations 404 pages - Removed in progress code for error handling - Removed default 'nl' locale - Removed redundant React import

--- src/lib/gql-client.ts
+++ src/lib/gql-client.ts
@@ -1,3 +1,22 @@
-import { QueryClient } from '@tanstack/query-core';
+import { QueryCache, QueryClient } from '@tanstack/query-core';
+import Router from 'next/router';
 
-export const gqlQueryClient = new QueryClient();
+export const gqlQueryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: (error, query) => {
+      Router.push({ pathname: '/404', query: { error: (error as Error).message } });
+
+      console.log('---');
+      console.log(error);
+      console.log(query);
+      console.log('---');
+    },
+  }),
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchIntervalInBackground: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
