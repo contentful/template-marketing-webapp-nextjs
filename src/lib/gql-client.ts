4d569b359bf1105f55f78ac9115c4f779aefc3c1
Matt van Voorst <mattvanvoorst@contentful.com>
2022-10-20 13:21:42 +0200

    chore: [] graphql Error handling - Tweaked the GraphQL api client, it now does not retry failed calls, neither on focus nor on an interval - On error, it now redirects pages to the 404 page.

--- src/lib/gql-client.ts
+++ src/lib/gql-client.ts
@@ -1,3 +1,17 @@
-import { QueryClient } from '@tanstack/query-core';
+import { QueryCache, QueryClient } from '@tanstack/query-core';
+import Router from 'next/router';
 
-export const gqlQueryClient = new QueryClient();
+export const gqlQueryClient = new QueryClient({
+  queryCache: new QueryCache({
+    onError: () => {
+      Router.push({ pathname: '/404' });
+    },
+  }),
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchIntervalInBackground: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
