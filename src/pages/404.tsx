8613774a458516cc6ed412fd101c2f528d3407ea
Matt van Voorst <mattvanvoorst@contentful.com>
2022-10-20 10:02:09 +0200

    fix: [] translations 404 pages - Fixed translations on the 404 page by statically rendering the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,23 @@
+import { GetStaticProps } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import PageError from '@src/components/errors/page-error';
+import { getServerSideTranslations } from '@src/lib/get-serverside-translations';
 
 const ErrorPage404 = () => {
+  const { query } = useRouter();
+  console.log(query);
+
   return <PageError error={{ code: 404 }} />;
+};
+
+export const getStaticProps: GetStaticProps = async ({ locale = 'nl' }) => {
+  return {
+    props: {
+      ...(await getServerSideTranslations(locale)),
+    },
+  };
 };
 
 export default ErrorPage404;
