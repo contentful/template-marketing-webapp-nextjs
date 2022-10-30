5a4f1a364e3600eaf2e20fafd0fbfb0e589c01f4
Nkoyo Ating <atingenkay@gmail.com>
2022-10-30 02:28:25 +0200

    PLATO-90: fix eslint import error

--- src/components/features/ctf-components/ctf-page/ctf-page-gql.tsx
+++ src/components/features/ctf-components/ctf-page/ctf-page-gql.tsx
@@ -2,8 +2,8 @@ import Head from 'next/head';
 
 import CtfPage from './ctf-page';
 
-import { PageError } from '@src/components/features/errors/page-error';
 import { useCtfPageQuery } from '@src/components/features/ctf-components/ctf-page/__generated/ctf-page.generated';
+import { PageError } from '@src/components/features/errors/page-error';
 import { useContentfulContext } from '@src/contentful-context';
 import { tryget } from '@src/utils';
 import contentfulConfig from 'contentful.config';
