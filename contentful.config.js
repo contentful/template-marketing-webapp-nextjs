b826ab4d3ed5d2f940f47ade349a6098f5313502
Remy Lenoir <remy.lenoir@contentful.com>
2022-11-08 17:05:40 +0100

    refactor: [PLATO-256] update env variables

--- contentful.config.js
+++ contentful.config.js
@@ -1,27 +1,24 @@
 // eslint-disable-next-line no-nested-ternary
-const url = !process.env.CONFIG_CONTENTFUL_META_URL
+const url = !process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_META_URL
   ? !process.env.VERCEL_URL
     ? 'http://localhost:3000'
     : `https://${process.env.VERCEL_URL}`
-  : process.env.CONFIG_CONTENTFUL_META_URL;
+  : process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_META_URL;
 
 module.exports = {
   contentful: {
-    // main_space_id: Contentful Space ID
-    main_space_id: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_MAIN_SPACE_ID || '',
-    // main_space_token: Contentful Space Delivery API - access token
-    main_space_token: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_MAIN_SPACE_TOKEN || '',
-    // main_space_preview_token: Contentful Space Preview API - access token
-    main_space_preview_token: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_MAIN_SPACE_PREVIEW_TOKEN || '',
+    space_id: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_SPACE_ID || '',
+    cda_token: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_DELIVERY_API_TOKEN || '',
+    cpa_token: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_PREVIEW_API_TOKEN || '',
     // main_space_management_token: Contentful Content Management token
     main_space_management_token: process.env.CONFIG_CONTENTFUL_MAIN_SPACE_MANAGEMENT_TOKEN || '',
   },
   meta: {
     title:
-      process.env.CONFIG_CONTENTFUL_META_TITLE ||
+      process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_META_TITLE ||
       'Digital banking for the new generation | Colorful Coin',
     description:
-      process.env.CONFIG_CONTENTFUL_META_DESCRIPTION ||
+      process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_META_DESCRIPTION ||
       `Enjoy premium banking services wherever you go: instant transfers and best exchange rates, exclusive offers and priority customer support. Apply online at ${url
         .replace('https://', '')
         .replace('http://', '')}`,
@@ -31,16 +28,16 @@ module.exports = {
   },
   icon: {
     light:
-      process.env.CONFIG_CONTENTFUL_ICON_LIGHT ||
+      process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_ICON_LIGHT ||
       'https://images.ctfassets.net/w8vf7dk7f259/llZXwDCnl9NqdyuVvjn1n/d20cea90225e7f53dfbf8a18a46e972d/gocoin-icon-light.svg',
     dark:
-      process.env.CONFIG_CONTENTFUL_ICON_DARK ||
+      process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_ICON_DARK ||
       'https://images.ctfassets.net/w8vf7dk7f259/i9iu6GU6dFWQJJwJzwxCT/952cc3bab415e28f521c22933072a09c/gocoin-icon.svg',
-    width: process.env.CONFIG_CONTENTFUL_ICON_WIDTH
-      ? parseInt(process.env.CONFIG_CONTENTFUL_ICON_WIDTH, 10)
+    width: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_ICON_WIDTH
+      ? parseInt(process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_ICON_WIDTH, 10)
       : 66,
-    height: process.env.CONFIG_CONTENTFUL_ICON_HEIGHT
-      ? parseInt(process.env.CONFIG_CONTENTFUL_ICON_HEIGHT, 10)
+    height: process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_ICON_HEIGHT
+      ? parseInt(process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_ICON_HEIGHT, 10)
       : 64,
   },
 };
