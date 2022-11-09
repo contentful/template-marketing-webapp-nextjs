eb3ce45303a47cee428e5c2d6378d6d3eac9f7cb
Remy Lenoir <remy.lenoir@contentful.com>
2022-11-09 11:18:46 +0100

    chore: [] remove unused types

--- src/utils.ts
+++ src/utils.ts
@@ -1,18 +1,3 @@
-export type Echo<T> = T;
-export type PartialNullable<T> = Partial<{ [K in keyof T]: T[K] | null }>;
-export type ArgumentTypes<F extends () => void> = F extends (...args: infer A) => any ? A : never;
-export type ArrayWithoutNulls<T extends any[]> = Exclude<T[0], null | undefined>[];
-export type NonNullValues<T> = {
-  [P in keyof T]: Exclude<T[P], null | undefined>;
-};
-export type RecursivePartial<T> = {
-  [P in keyof T]?: T[P] extends (infer U)[]
-    ? RecursivePartial<U>[]
-    : T[P] extends object
-    ? RecursivePartial<T[P]>
-    : T[P];
-};
-
 type OmitCustom<T, K> = Pick<T, Exclude<keyof T, K>>;
 export type OmitDistributive<T, K> = T extends any
   ? T extends object
