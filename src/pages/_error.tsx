b35bc53bb0bb5b50e0ea9d5fde735772cd906aa3
Stephan Leece <stephan.leece@contentful.com>
2022-11-22 17:18:54 +0100

    update error page

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,9 +1,4 @@
 /**
- * NOTE: This requires `@sentry/nextjs` version 7.3.0 or higher.
- *
- * NOTE: If using this with `next` version 12.2.0 or lower, uncomment the
- * penultimate line in `CustomErrorComponent`.
- *
  * This page is loaded by Nextjs:
  *  - on the server, when data-fetching methods throw or reject
  *  - on the client, when `getInitialProps` throws or rejects
@@ -20,10 +15,6 @@ import * as Sentry from '@sentry/nextjs';
 import NextErrorComponent from 'next/error';
 
 const CustomErrorComponent = props => {
-  // If you're using a Nextjs version prior to 12.2.1, uncomment this to
-  // compensate for https://github.com/vercel/next.js/issues/8592
-  // Sentry.captureUnderscoreErrorException(props);
-
   return <NextErrorComponent statusCode={props.statusCode} />;
 };
 
