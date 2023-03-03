31db788d48480dea94f931032ec2d69086413bf5
Kudakwashe Mupeni <terrencemupeni@gmail.com>
2023-03-03 14:23:17 +0100

    feat: add support for preview content

--- src/lib/fetchConfig.ts
+++ src/lib/fetchConfig.ts
@@ -5,7 +5,7 @@ export const fetchConfig = {
   params: {
     headers: {
       'Content-Type': 'application/json',
-      Authorization: `Bearer ${process.env.CONTENTFUL_ACCESS_TOKEN}`,
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_CONFIG_CONTENTFUL_PREVIEW_API_TOKEN}`,
     },
   },
 };
